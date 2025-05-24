e3a6a043af4a1060c7522c2a4547a54f6387cf1a
DalineSeritsane <dalinelerato@gmail.com>
2025-05-24 05:50:40 +0200

    admin and sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./sidebar.css"
+import { Link } from 'react-router-dom'
+import add_product_icon from "../../assets/cart.jpg";
+
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      
+      <Link to={'/addproduct'} style={{textDecoration:"none"}}>
+      <div className='sidebar-item'>
+        <img src={add_product_icon} alt='' />
+      </div>
+      </Link>
     </div>
   )
 }
